f881c10e6f7d7166ab003c51131b248049c7ee60
eb <eiler.byberi@gmail.com>
2022-06-28 19:10:50 -0700

    Add the code for the Caesar Cipher

---
+++ Caesar_Cipher.rb
@@ -0,0 +1,20 @@
+def caesar_cipher(string, shift)
+  lower_case_array = ('a'..'z').to_a.reverse
+  upper_case_array = ('A'..'Z').to_a.reverse
+
+  new_string = string.split(//).map do |char|
+    if (char.ord > 96 && char.ord < 123)
+      new_index = lower_case_array.index(char) - shift
+      char = lower_case_array[new_index]
+    elsif (char.ord < 91 && char.ord > 40) 
+      new_index = upper_case_array.index(char) - shift
+      char = upper_case_array[new_index]
+    else
+      char = char
+    end
+  end
+  new_string = new_string.join
+  puts new_string
+end
+
+caesar_cipher("What a string!", 5)
