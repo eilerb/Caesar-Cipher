da0bb5d78f6fd14f70075aa9a9ea699a1d678bcc
eb <eiler.byberi@gmail.com>
2022-07-08 19:41:10 -0700

    Fix formatting

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -3,10 +3,10 @@ def caesar_cipher(string, shift)
   upper_case_array = ('A'..'Z').to_a.reverse
 
   new_string = string.split(//).map do |char|
-    if (char.ord > 96 && char.ord < 123)
+    if char.ord > 96 && char.ord < 123
       new_index = lower_case_array.index(char) - shift
       char = lower_case_array[new_index]
-    elsif (char.ord < 91 && char.ord > 40) 
+    elsif char.ord < 91 && char.ord > 40
       new_index = upper_case_array.index(char) - shift
       char = upper_case_array[new_index]
     else
@@ -17,4 +17,4 @@ def caesar_cipher(string, shift)
   puts new_string
 end
 
-caesar_cipher("What a string!", 5)
+caesar_cipher('What a string!', 5)
